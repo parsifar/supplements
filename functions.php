6e8795f8cef98b8b0a563d2a20509d5185c392ff
parsifar <parsifar@gmail.com>
2025-04-20 12:44:46 -0400

    blank child theme

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Functions
+ *
+ * @package SuppPick
+ */
+
+// Enqueue parent and child theme styles.
+add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_styles' );
+
+
+function astra_child_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
