a5971e6fadc8b13c5064580a089135331ab17641
parsifar <parsifar@gmail.com>
2025-04-20 23:12:45 -0400

    add tax query

--- functions.php
+++ functions.php
@@ -5,10 +5,87 @@
  * @package SuppPick
  */
 
-// Enqueue parent and child theme styles.
 add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_styles' );
-
-
 function astra_child_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/dist/style.css', array(), false, 'all' );
 }
+
+
+function filter_supplement_query( $query ) {
+	if ( is_admin() || ! $query->is_main_query() || ! is_post_type_archive( 'supplement' ) ) {
+		return;
+	}
+
+	// Taxonomy filters.
+	$taxonomies = array( 'brand', 'certification', 'dietary-tag', 'product-form', 'supplement-category' );
+	$tax_query  = array();
+
+	foreach ( $taxonomies as $taxonomy ) {
+		if ( ! empty( $_GET[ $taxonomy ] ) ) {
+			$tax_query[] = array(
+				'taxonomy' => $taxonomy,
+				'field'    => 'slug',
+				'terms'    => sanitize_text_field( $_GET[ $taxonomy ] ),
+			);
+		}
+	}
+
+	if ( ! empty( $tax_query ) ) {
+		$query->set(
+			'tax_query',
+			array(
+				'relation' => 'AND',
+				...$tax_query,
+			)
+		);
+	}
+
+	// Meta filters.
+	$meta_query = array();
+
+	if ( ! empty( $_GET['max_price'] ) ) {
+		$meta_query[] = array(
+			'key'     => 'price',
+			'value'   => floatval( $_GET['max_price'] ),
+			'compare' => '<=',
+			'type'    => 'DECIMAL',
+		);
+	}
+
+	if ( ! empty( $_GET['max_pps'] ) ) {
+		$meta_query[] = array(
+			'key'     => 'price_per_serving',
+			'value'   => floatval( $_GET['max_pps'] ),
+			'compare' => '<=',
+			'type'    => 'DECIMAL',
+		);
+	}
+
+	if ( ! empty( $meta_query ) ) {
+		$query->set( 'meta_query', $meta_query );
+	}
+
+	// Sorting.
+	$sort = sanitize_text_field( $_GET['sort'] ?? '' );
+	switch ( $sort ) {
+		case 'price_asc':
+			$query->set( 'orderby', 'meta_value_num' );
+			$query->set( 'meta_key', 'price' );
+			$query->set( 'order', 'ASC' );
+			break;
+		case 'price_desc':
+			$query->set( 'orderby', 'meta_value_num' );
+			$query->set( 'meta_key', 'price' );
+			$query->set( 'order', 'DESC' );
+			break;
+		case 'name_asc':
+			$query->set( 'orderby', 'title' );
+			$query->set( 'order', 'ASC' );
+			break;
+		case 'name_desc':
+			$query->set( 'orderby', 'title' );
+			$query->set( 'order', 'DESC' );
+			break;
+	}
+}
+add_action( 'pre_get_posts', 'filter_supplement_query' );
